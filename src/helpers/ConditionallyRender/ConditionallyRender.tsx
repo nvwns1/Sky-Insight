ad862eb1044df31a8dee333549fb1f3d95abe5de
nvwns1 <nvwns12345@gmail.com>
2024-03-10 11:24:24 +0545

    Added Helpers

---
+++ src/helpers/ConditionallyRender/ConditionallyRender.tsx
@@ -0,0 +1,42 @@
+import React, { ReactNode } from 'react'
+interface IConditionallyRender {
+    condition: boolean;
+    show: TargetElement;
+    elseShow?: TargetElement;
+}
+type TargetElement = JSX.Element | JSX.Element[] | RenderFunc | ReactNode | null;
+type RenderFunc = () => JSX.Element | null;
+
+const ConditionallyRender = ({ condition, show, elseShow }: IConditionallyRender): JSX.Element | null => {
+    const handleFunction = (renderFunc: RenderFunc): JSX.Element | null => {
+        const result = renderFunc();
+        if (!result) {
+            // eslint-disable-next-line no-console
+            console.warn(
+                'Nothing was returned from your render function. Verify that you are returning a valid react component'
+            );
+            return null;
+        }
+        return result;
+    };
+
+    const isFunc = (param: TargetElement): boolean => {
+        return typeof param === 'function';
+    };
+    if (condition) {
+        if (isFunc(show)) {
+            return handleFunction(show as RenderFunc)
+        }
+        return show as JSX.Element
+    }
+    if (!condition && elseShow) {
+        if (isFunc(elseShow)) {
+            return handleFunction(elseShow as RenderFunc);
+        }
+        return elseShow as JSX.Element;
+    }
+
+    return null
+}
+
+export default ConditionallyRender
